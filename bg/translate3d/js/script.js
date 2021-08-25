edcf92fbad82ed39cbfe751e004f0f13053a2289
JANGDEOKSIK <tessjds@easymedia.net>
2021-08-25 12:09:38 +0900

    translate3d 효과 추가

---
+++ bg/translate3d/js/script.js
@@ -0,0 +1,15 @@
+$(function(){
+    var m = 1.5;
+    var w,h,t;
+    w = $(".bg").width();
+    h = $(".bg").height();
+    t = $(".bg").offset().top;
+    $(window).on("mousemove", function(e){
+        var horTilt = ((e.clientX / w) * (m * 2)) - m;
+        var verTilt = (((e.clientY - t) / h) * (m * 2)) - m;					
+        TweenMax.to('.bg', 1,{rotationY:horTilt, rotationX:-verTilt, scale: 1.05, ease:Power1.easeOut});
+        console.log("rotationX : " + verTilt);
+        console.log("rotationY : " + horTilt);
+        console.log("")
+    })
+})
