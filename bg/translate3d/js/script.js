d3cb316205a58c40bcd05d8d5831faaa18e217f0
JANGDEOKSIK <tessjds@easymedia.net>
2021-08-25 12:19:08 +0900

    translate3d 수정

--- bg/translate3d/js/script.js
+++ bg/translate3d/js/script.js
@@ -7,9 +7,8 @@ $(function(){
     $(window).on("mousemove", function(e){
         var horTilt = ((e.clientX / w) * (m * 2)) - m;
         var verTilt = (((e.clientY - t) / h) * (m * 2)) - m;					
-        TweenMax.to('.bg', 1,{rotationY:horTilt, rotationX:-verTilt, scale: 1.05, ease:Power1.easeOut});
-        console.log("rotationX : " + verTilt);
-        console.log("rotationY : " + horTilt);
-        console.log("")
+        gsap.to('.bg', {duration: 1, rotationY:horTilt, rotationX:-verTilt, scale: 1.05, ease:Power1.easeOut});
+        // console.log("rotationX : " + verTilt);
+        // console.log("rotationY : " + horTilt);
     })
 })
